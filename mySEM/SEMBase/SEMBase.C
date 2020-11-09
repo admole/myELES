84577a10ec852df51019df5e92ce929c19ca3d95
Andrew Mole <andrew.mole@postgrad.manchester.ac.uk>
2020-11-09 07:52:44 +0000

    base value fraction on instantaneous velocity

--- mySEM/SEMBase/SEMBase.C
+++ mySEM/SEMBase/SEMBase.C
@@ -468,14 +468,14 @@ void SEMBase::updateCoeffs()
         
         this->correctMass();
 
-        label patchIndex = this->patch().index();
+        //label patchIndex = this->patch().index();
         //const label patchId = mesh.boundaryMesh().findPatchID("INLET");
 
-        const surfaceScalarField & phi = this->db().objectRegistry::lookupObject<surfaceScalarField>(phiName_);
-        const scalarField & phip = phi.boundaryField()[patchIndex];
+        //const surfaceScalarField & phi = this->db().objectRegistry::lookupObject<surfaceScalarField>(phiName_);
+        //const scalarField & phip = phi.boundaryField()[patchIndex];
 
-        this->valueFraction() = 1.0 - pos0(phip);
-        //this->valueFraction() = 1.0 - pos0((meanField_ & this->patch().nf()) / mag(meanField_ & this->patch().nf()));
+        //this->valueFraction() = 1.0 - pos0(phip);
+        this->valueFraction() = 1.0 - pos0((this->refValue() & this->patch().nf()) / mag(this->refValue() & this->patch().nf()));
 
         curTimeIndex_ = this->db().time().timeIndex();
     }
